9afcd6e578f477986cb43600c56c388d061da474
royber <royber@yardim.com.br>
2020-02-28 13:50:46 -0300

    create model 3

---
+++ WebGitTeste3/Models/Model3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGitTeste3.Models
+{
+    public class Model3
+    {
+        public int id { get; set; }
+    }
+}
